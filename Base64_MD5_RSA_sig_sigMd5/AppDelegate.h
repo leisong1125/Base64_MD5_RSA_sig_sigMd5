09bccb5918e56cd281b48f0b432ac5281f6f0ade
FanLang <fanlang@fanlangdemacbook-air.local>
2016-05-12 16:09:22 +0800

    Initial Commit

---
+++ Base64_MD5_RSA_sig_sigMd5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Base64_MD5_RSA_sig_sigMd5
+//
+//  Created by FanLang on 16/5/12.
+//  Copyright © 2016年 FanLang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Base64_MD5_RSA_sig_sigMd5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Base64_MD5_RSA_sig_sigMd5
+//
+//  Created by FanLang on 16/5/12.
+//  Copyright © 2016年 FanLang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
